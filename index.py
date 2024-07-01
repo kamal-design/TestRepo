06e7a9a1dce1819ae1bd203575c3bed821facfd4
kamaledvoy <smkamal.sm@gmail.com>
2024-07-01 10:03:09 +0530

    python basics

---
+++ index.py
@@ -0,0 +1,20 @@
+hi = "Hello World!"
+a = 9
+b = 6
+
+c = 6+7
+# command Line
+print(a+b)
+print(c)
+print(hi)
+
+a = 9
+if a > 4:
+    print("Data")
+else:
+    print("welcome")
+
+#ternary operator
+print('first') if a>4 else print('second')
+
+# Data Types
